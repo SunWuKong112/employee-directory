5463f3c38abfeb2d1ba3bcf71a71d7b20cbeae2c
Kyle Gribble <dribbleGribble0516@gmail.com>
2020-10-02 21:44:01 -0500

    Updates

---
+++ precache-manifest.127979f7c17750822408f3efc1bcd1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f60463e62151ea6e92a20085af4fbdb",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "495d92f201c05678a1c0",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "15aeff3bad6b01040b34",
+    "url": "/my-app/static/js/2.e8b4d229.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e8b4d229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495d92f201c05678a1c0",
+    "url": "/my-app/static/js/main.834619aa.chunk.js"
+  },
+  {
+    "revision": "0a84e5ca07b1a2df8506",
+    "url": "/my-app/static/js/runtime-main.7ff7a6dd.js"
+  },
+  {
+    "revision": "26ef1cbcfa05495818a9d63927b452cb",
+    "url": "/my-app/static/media/logo.26ef1cbc.svg"
+  }
+]);
