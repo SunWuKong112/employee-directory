22d619979fbf7e6ab2bd0c130660ab00af77dd5c
Kyle Gribble <dribbleGribble0516@gmail.com>
2020-10-02 21:11:59 -0500

    Fixing deployed appliation

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, useLocation} from "react-router-dom";
+// import {Link, useLocation} from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavTabs(props){
